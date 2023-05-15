e64f96a29ba67c4d5eb33d0825857beab80b47b4
Phu Plus <ttphuem2k1@gmail.com>
2023-05-15 10:59:06 +0700

    change vn

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const cardImg = document.querySelector('.card_img');
+const cardImg = document.querySelector(".card_img");
 
-cardImg.addEventListener('click', function() {
-  if (cardImg.classList.contains('fullscreen')) {
+cardImg.addEventListener("click", function () {
+  if (cardImg.classList.contains("fullscreen")) {
     // Nếu có lớp "fullscreen", xóa lớp và trả lại kích thước ban đầu
-    cardImg.classList.remove('fullscreen');
+    cardImg.classList.remove("fullscreen");
   } else {
     // Nếu không có lớp "fullscreen", thêm lớp để kích hoạt chế độ toàn màn hình
-    cardImg.classList.add('fullscreen');
+    cardImg.classList.add("fullscreen");
   }
 });
